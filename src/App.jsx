a0b0a24ab8f29254baa3474d11c5513da22ade61
Juliana Ifionu <julieifionu@gmail.com>
2024-03-01 13:44:59 -0500

    Add header component and style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,18 @@
 import './App.css';
+import Header from './component/Header';
+import NavBar from './component/NavBar';
 import Button from './component/Button';
 import DateInput from './component/DateInput';
 import Select from './component/Select';
+import Filter from './component/Filter';
 
 function App() {
 	return (
-		<main className="Ap font-heading-large">
-			hello world
-			<div>
-				<Button type="add-invoice" onClick={() => console.log('added 1 item')}>
-					<div className="add-button-content font-bold">
-						<div className="add-button-content-right">
-							<span>+</span>
-						</div>
-						<span className="add-button-content-left">New Invoice</span>
-					</div>
-				</Button>
-
-				<Button type="add-item" onClick={() => console.log('added 1 item')}>
-					<span className="font-bold">+ Add New Item</span>
-				</Button>
-
-				<Button type="edit-dark" onClick={() => console.log('added 1 item')}>
-					<span className="font-bold">Edit</span>
-				</Button>
-
-				<Button type="edit-light" onClick={() => console.log('added 1 item')}>
-					<span className="font-bold">Edit</span>
-				</Button>
-				<Select />
-				<DateInput />
-			</div>
+		<main className="app-container font-heading-large">
+			<NavBar />
+			<section className="app-wrapper">
+				<Header />
+			</section>
 		</main>
 	);
 }
---
+++ src/component/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../styles/Header.css';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
+import Button from './Button';
+import Filter from './Filter';
+
+function Header() {
+	return (
+		<div className="header-container">
+			<div className="header-left">
+				<h2 className="font-heading-md">Invoices</h2>
+				<span className="font-opacity">7 invoices</span>
+			</div>
+			<div className="header-right">
+				<div>
+					<Filter />
+				</div>
+				<div>
+					<Button
+						type="add-invoice"
+						onClick={() => console.log('added 1 item')}
+					>
+						<div className="add-button-content font-bold">
+							<div className="add-button-content-right">
+								<span>+</span>
+							</div>
+							<span className="add-button-content-left">New</span>
+						</div>
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
